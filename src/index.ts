8ebf562d68ad2a5d523430859bd8aa4f8174d1fb
Tyler Long <tyler4long@gmail.com>
2020-05-28 11:26:32 -0700

    Only support RC SDK V4

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import {WsgOptions} from 'ringcentral-unified/build/Wsg';
 import Unified from 'ringcentral-unified';
+import RingCentral, {AuthData} from '@ringcentral/sdk';
+import {TokenInfo} from 'ringcentral-unified/build/definitions';
+import {events} from '@ringcentral/sdk/lib/platform/Platform';
 
-type RingCentral = {
-  platform: () => {
-    loginUrl: () => string;
-    auth: () => {
-      data: () => {};
-    };
+function authData2TokenInfo(authData: AuthData): TokenInfo {
+  return {
+    ...authData,
+    expires_in: authData.expires_in ? parseInt(authData.expires_in) : undefined,
+    refresh_token_expires_in: authData.refresh_token_expires_in
+      ? parseInt(authData.refresh_token_expires_in)
+      : undefined,
   };
-};
+}
 
 export default class WSG {
   unified?: Unified;
@@ -22,15 +26,27 @@ export default class WSG {
       .platform()
       .loginUrl()
       .split('/restapi/oauth/authorize')[0];
-    const token = await rc.platform().auth().data();
+    const authData = await rc.platform().auth().data();
     this.unified = new Unified(
       {
         server,
-        token,
+        token: authData2TokenInfo(authData),
       },
       this.wsgOptions
     );
-    // todo: auto set token if token updated
+    rc.platform().on(events.loginSuccess, async () => {
+      this.unified!.token = authData2TokenInfo(
+        await rc.platform().auth().data()
+      );
+    });
+    rc.platform().on(events.refreshSuccess, async () => {
+      this.unified!.token = authData2TokenInfo(
+        await rc.platform().auth().data()
+      );
+    });
+    rc.platform().on(events.logoutSuccess, async () => {
+      await this.revoke();
+    });
   }
 
   async subscribe(eventFilters: string[], callback: (body: {}) => void) {
